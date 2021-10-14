d4c03c8fc101f0617f1b7830d750dd8564eaba66
Griffin Desktop <gjourda@gmail.com>
2021-10-14 11:32:05 -0400

    added twobody propagator

--- pyOrbital/constants.py
+++ pyOrbital/constants.py
@@ -1,8 +1,9 @@
 # https://ssd.jpl.nasa.gov/?constants
 
-G = 6.67430e-11     # universal gravitational constant - m^3/(kg*s^2)
-au = 149597870700   # one AU - m
-g0 = 9.80665        # Earth standard gravity - m/s^2
-j2000 = 2451545.0   # J2000 epoch julian date (days)
-re = 6378e3         # Earth radius (m)
+G = 6.67430e-11         # universal gravitational constant - m^3/(kg*s^2)
+au = 149597870700       # one AU - m
+g0 = 9.80665            # Earth standard gravity - m/s^2
+j2000 = 2451545.0       # J2000 epoch julian date (days)
+re = 6378e3             # Earth radius (m)
 mu = 3.986004418e14     # Earth mu (m^3/s^2)
+j2 = 1.08262668e-3      # Earth J2 constant
