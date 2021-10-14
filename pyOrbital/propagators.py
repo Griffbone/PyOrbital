d8f787a36cc4aa7ed9246efe214c9b51eb1a1420
Griffin Laptop <gjourda@gmail.com>
2021-10-14 18:42:51 -0400

    added j2 propagator and propagator docstrings

--- pyOrbital/propagators.py
+++ pyOrbital/propagators.py
@@ -3,6 +3,7 @@ from scipy.optimize import fsolve
 from scipy.integrate import solve_ivp
 import constants as cons
 import matplotlib.pyplot as plt
+import functions as func
 
 # STK propagators: https://help.agi.com/stk/index.htm#stk/vehSat_orbitProp_choose.htm
 # IITB propagators: https://www.aero.iitb.ac.in/satelliteWiki/index.php/Orbit_Propagator
@@ -25,9 +26,13 @@ def kepler_ea(e, ma):
     return fsolve(lambda E: E - e*np.sin(E) - ma, np.array([0]))
 
 
-def twobody_propagation(r, v, dt, n=1000):
+def twobody_propagation(r, v, dt, n=1000, step=1e3):
     """ Propagate a satellite using two body propagation
-
+        :param r: initial position (m)
+        :param v: initial velocity (m/s)
+        :param dt: end time of simulation (s)
+        :param n: number of data points to return
+        :param step: maximum step size (s)
     """
 
     def state_dot(time, state):
@@ -36,7 +41,7 @@ def twobody_propagation(r, v, dt, n=1000):
         r = np.array([x, y, z])
         rm = np.linalg.norm(r)
 
-        gx = -((cons.mu*x)/rm)*(1 + 1.5*cons.j2)
+        g = -(cons.mu*r)/rm**3
 
         vx_dot = g[0]
         vy_dot = g[1]
@@ -45,7 +50,7 @@ def twobody_propagation(r, v, dt, n=1000):
         return np.array([x_dot, y_dot, z_dot, vx_dot, vy_dot, vz_dot])
 
     state_0 = np.array([r[0], r[1], r[2], v[0], v[1], v[2]])
-    sol = solve_ivp(state_dot, [0, dt], state_0, 'DOP853', t_eval=np.linspace(0, dt, n))
+    sol = solve_ivp(state_dot, [0, dt], state_0, 'DOP853', t_eval=np.linspace(0, dt, n), max_step=step)
 
     t = sol.t
     y = sol.y
@@ -57,31 +62,28 @@ def twobody_propagation(r, v, dt, n=1000):
     return t, xs, ys, zs
 
 
-t, x, y, z = twobody_propagation([cons.re, 0, 0], [0, 8000, 250], 90*60, n=10000)
-plt.plot(x, y)
-plt.axis('equal')
-plt.show()
-
-
-def j2_propagation(r, v, dt, n=1000):
-    """ Propagate a satellite using two body propagation
-
+def j2_propagation(r, v, dt, n=1000, step=1e3):
+    """ Propagate a satellite using two body propagation with J2 perturbation
+        :param r: initial position (m)
+        :param v: initial velocity (m/s)
+        :param dt: end time of simulation (s)
+        :param n: number of data points to return
+        :param step: maximum step size (s)
     """
 
     def state_dot(time, state):
         x, y, z, x_dot, y_dot, z_dot = state
 
-        r = np.array([x, y, z])
-        g = -(cons.mu*r)/np.linalg.norm(r)**3
+        r = np.sqrt(x**2 + y**2 + z**2)
 
-        vx_dot = g[0]
-        vy_dot = g[1]
-        vz_dot = g[2]
+        gx = -(cons.mu*x/(r**3))*(1 + 1.5*cons.j2*(cons.re**2/r**2) - 7.5*cons.j2*(cons.re**2*z**2)/r**4)
+        gy = -(cons.mu*y/(r**3))*(1 + 1.5*cons.j2*(cons.re**2/r**2) - 7.5*cons.j2*(cons.re**2*z**2)/r**4)
+        gz = -(cons.mu*z/(r**3))*(1 + 4.5*cons.j2*(cons.re**2/r**2) - 7.5*cons.j2*(cons.re**2*z**2)/r**4)
 
-        return np.array([x_dot, y_dot, z_dot, vx_dot, vy_dot, vz_dot])
+        return np.array([x_dot, y_dot, z_dot, gx, gy, gz])
 
     state_0 = np.array([r[0], r[1], r[2], v[0], v[1], v[2]])
-    sol = solve_ivp(state_dot, [0, dt], state_0, 'DOP853', t_eval=np.linspace(0, dt, n))
+    sol = solve_ivp(state_dot, [0, dt], state_0, 'DOP853', t_eval=np.linspace(0, dt, n), max_step=step)
 
     t = sol.t
     y = sol.y
@@ -89,5 +91,8 @@ def j2_propagation(r, v, dt, n=1000):
     xs = y[0, :]
     ys = y[1, :]
     zs = y[2, :]
+    vxs = y[3, :]
+    vys = y[4, :]
+    vzs = y[5, :]
 
-    return t, xs, ys, zs
+    return t, xs, ys, zs, vxs, vys, vzs
