040eaf7a4f4487659b591f805faba775a04c8c7c
Griffin Laptop <gjourda@gmail.com>
2021-11-01 17:44:53 -0400

    added wiki file, reorganized functions file

--- plotting.py
+++ plotting.py
@@ -15,7 +15,7 @@ def plot_orbit(a, e, i, omega, w, tas=np.linspace(0, 2*np.pi, 100)):
         :return z: vector of z positions
     """
 
-    _, _, x, y = perifocal_coords(a, e, tas)
+    _, _, x, y = func.perifocal_coords(a, e, tas)
     z = np.zeros(len(x))
 
     xps = []
@@ -24,11 +24,33 @@ def plot_orbit(a, e, i, omega, w, tas=np.linspace(0, 2*np.pi, 100)):
 
     for x, y, z in zip(x, y, z):
         r = np.array([x, y, z])
-        xp, yp, zp = perifocal_to_eci(omega, i, w, r)
+        xp, yp, zp = func.perifocal_to_eci(omega, i, w, r)
 
         xps.append(xp)
         yps.append(yp)
         zps.append(zp)
 
     return xps, yps, zps
+
+
+def ground_track(t0, ts, xs, ys, zs):
+    """ Function to return geodetic lat/lon from ECI positions
+        :param t0: epoch (JDN)
+        :param ts: array of times since epoch (s)
+        :param xs: ECI X position
+        :param ys: ECI Y position
+        :param zs: ECI Z position
+    """
+
+    phis = []
+    lams = []
+
+    for i in range(0, len(ts)):
+        t = ts[i]/60**2
+        phi, lam = func.eci_to_ll(xs[i], ys[i], zs[i], t0, t)
+
+        phis.append(phi)
+        lams.append(lam)
+
+    return phis, lams
 
