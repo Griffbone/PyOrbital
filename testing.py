32bf50fd1c91804ca767ff31dd743b3b4792e751
Griffin Laptop <gjourda@gmail.com>
2021-11-15 20:36:05 -0500

    lunar patched conics

--- testing.py
+++ testing.py
@@ -30,24 +30,36 @@ soi = 66.1e6
 sat = func.Elements((ra + rp)/2, (ra - rp)/(ra + rp), 0, 0, 0, 0, cns.mu)
 moon = func.Elements(0.3844e9, 0, 0, 0, 0, 140, cns.mu)
 
-#############################################################################
-ts, xm, ym, zm = propagators.kepler_propagation(moon.a, moon.e, moon.i, 0, 0, moon.ta, sat.T/2)
-_, xs, ys, zs = propagators.kepler_propagation(sat.a, sat.e, sat.i, 0, 0, sat.ta, max(ts))
-rs = np.sqrt((xs - xm)**2 + (ys - ym)**2 + (zs - zm)**2)
+mu_m = 4.9028e3*1000**3
 
-tas, tam, _ = (dev_functions.patch_point(sat, moon, 66.1e6))
-Dt = func.t_between(sat.a, sat.e, sat.ta, tas, sat.mu)
 
-print(tas)
+def lunar_pc(r0, v0, phi0, lam1, rs=soi, rm=ra):
+    # transfer orbit conditions
+    E = v0**2/cns.mu - cns.mu/r0**2
+    h = v0*r0*np.sin(phi0)
 
-x, y, _ = sat.get_eci(tas)
-plt.scatter(x, y)
+    # patch point conditions
+    r1 = np.sqrt(rs**2 + rm**2 - 2*rs*rm*np.cos(lam1))
+    v1 = np.sqrt(2*(E + cns.mu/r1))
+    phi1 = np.arccos(h/(r1*v1))
+    gam1 = np.arcsin((rs/r1) * np.sin(lam1))
 
-x, y, _ = moon.get_eci(tam)
-plt.scatter(x, y)
-circle(soi, x, y, 'k--')
+    # selenocentric orbit
+    vm = np.sqrt(cns.mu/rm)
+    v2 = np.sqrt(v1**2 + vm**2 - 2*v1*vm*np.cos(phi1 - gam1))
+    eps2 = np.sin((vm/v2)*np.cos(lam1) - (v1/v2)*np.cos(lam1 + gam1 - phi1))
 
-sat.perifocal_plot()
-moon.perifocal_plot()
+    h2 = v2*rs*np.sin(eps2)
+    E = v2**2/2 - mu_m/rs
+    p = h2**2/mu_m
 
-plt.show()
+    e = np.sqrt(1 + 2*E*h**2/mu_m**2)
+    rp = p/(1 + e)
+
+    print(rp)
+
+
+print(soi)
+print(ra)
+
+lunar_pc(6697e3, 10.8462e3, 0, np.deg2rad(30))
