0500f067b1d9973b4296c65c04bb950a2610fbbb
Griffin Desktop <gjourda@gmail.com>
2021-11-16 11:29:47 -0500

    modified lunar PC function

--- testing.py
+++ testing.py
@@ -30,13 +30,13 @@ soi = 66.1e6
 sat = func.Elements((ra + rp)/2, (ra - rp)/(ra + rp), 0, 0, 0, 0, cns.mu)
 moon = func.Elements(0.3844e9, 0, 0, 0, 0, 140, cns.mu)
 
-mu_m = 4.9028e3*1000**3
+mu_m = 4.9048695e12
 
 
 def lunar_pc(r0, v0, phi0, lam1, rs=soi, rm=ra):
     # transfer orbit conditions
-    E = v0**2/cns.mu - cns.mu/r0**2
-    h = v0*r0*np.sin(phi0)
+    E = v0**2/2 - cns.mu/r0
+    h = v0*r0*np.cos(phi0)
 
     # patch point conditions
     r1 = np.sqrt(rs**2 + rm**2 - 2*rs*rm*np.cos(lam1))
@@ -47,19 +47,41 @@ def lunar_pc(r0, v0, phi0, lam1, rs=soi, rm=ra):
     # selenocentric orbit
     vm = np.sqrt(cns.mu/rm)
     v2 = np.sqrt(v1**2 + vm**2 - 2*v1*vm*np.cos(phi1 - gam1))
-    eps2 = np.sin((vm/v2)*np.cos(lam1) - (v1/v2)*np.cos(lam1 + gam1 - phi1))
+    eps2 = np.arcsin((vm/v2)*np.cos(lam1) - (v1/v2)*np.cos(lam1 + gam1 - phi1))
 
-    h2 = v2*rs*np.sin(eps2)
+    h2 = v2*rs*abs(np.sin(eps2))
     E = v2**2/2 - mu_m/rs
     p = h2**2/mu_m
 
     e = np.sqrt(1 + 2*E*h**2/mu_m**2)
     rp = p/(1 + e)
+    vp = h2/rp
 
-    print(rp)
+    vpe = np.sqrt(2*(E + mu_m/rp))
+    print([vp, vpe])
+
+    print(vp**2/2 - mu_m/rp)
+    print(E)
+
+    return rp, vp
 
 
-print(soi)
-print(ra)
+lunar_pc(cns.re + 200e3, np.sqrt(cns.mu/(cns.re + 200e3)) + 3200, 0, np.deg2rad(19))
 
-lunar_pc(6697e3, 10.8462e3, 0, np.deg2rad(30))
+# ls = np.linspace(17, 19, 1000)
+# rps = []
+# dvs = []
+#
+# for l in ls:
+#     rp, vp = lunar_pc(cns.re + 200e3, np.sqrt(cns.mu/(cns.re + 200e3)) + 3200, 0, np.deg2rad(l))
+#     dv = vp - np.sqrt(mu_m/rp)
+#
+#     rps.append(rp)
+#     dvs.append(vp)
+
+# print(np.sqrt(mu_m/1737447.78))
+
+# plt.plot(ls, (np.array(rps) - 1737447.78)/1000)
+# plt.plot(ls, np.array(dvs))
+# # yline(100, max(ls))
+# plt.show()
