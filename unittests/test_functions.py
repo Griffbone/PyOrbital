ccf0779349f99525dc772219584e88626e8b6d25
Griffin Desktop <gjourda@gmail.com>
2021-11-22 18:01:40 -0500

    finally added unit tests

---
+++ unittests/test_functions.py
@@ -0,0 +1,26 @@
+import unittest
+import functions as func
+
+
+class TestFunctions(unittest.TestCase):
+    def test_vector_to_elements(self):
+        # circular equatorial
+
+        # circular inclined
+
+        # elliptical equatorial
+
+        # elliptical inclined
+
+        # parabolic
+
+        # hyperbolic
+
+        pass
+
+    def test_elements_to_vector(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
